b0d26cbacdef5313f62a79a35de41e38abd31efd
BIT-USER <BIT-USER@DESKTOP-81R6F60>
2018-01-10 09:37:57 +0900

    1st, 2nd, 3rd. solved.

--- src/com/javaex/problem02/Depart.java
+++ src/com/javaex/problem02/Depart.java
@@ -2,6 +2,22 @@ package com.javaex.problem02;
 
 public class Depart extends Employee {
 
-    //코드작성
+	private String name;
+	private int salary;
+	private String department;
+
+	// 코드작성
+	public Depart() {
+	}
+
+	public Depart(String name, int salary, String department) {
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public void getInformation() {
+		System.out.println("이름:" + name + " 연봉:" + salary + " 부서:" + department);
+	}
 
 }
--- src/com/javaex/problem02/EmployeeApp.java
+++ src/com/javaex/problem02/EmployeeApp.java
@@ -2,10 +2,10 @@ package com.javaex.problem02;
 
 public class EmployeeApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Employee pt = new Depart( "홍길동", 5000, "개발부" );
-        pt.getInformation();
-    }
+		Employee pt = new Depart("홍길동", 5000, "개발부");
+		pt.getInformation();
+	}
 
 }
